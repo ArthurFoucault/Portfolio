070d5f7c11834a2d1b72339af1738031a3c6bb0b
Arthur Foucault <afo@mtb111.fr>
2023-07-12 09:34:49 +0200

    page simple

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-    echo "Hello github";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ma première page Web</title>
+</head>
+<body>
+  <h1>Bienvenue sur ma page Web !</h1>
+  <p>Ceci est un paragraphe.</p>
+  <p>Voici une liste non ordonnée :</p>
+  <ul>
+    <li>Élément 1</li>
+    <li>Élément 2</li>
+    <li>Élément 3</li>
+  </ul>
+</body>
+</html>
