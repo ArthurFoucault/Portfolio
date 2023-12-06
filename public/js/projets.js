12689b4dfab2e8e5ea16eed6e4c9b966c6a79aed
Arthur Foucault <afo@mtb111.fr>
2023-12-07 00:23:06 +0100

    ajout sujet veille

---
+++ public/js/projets.js
@@ -0,0 +1,59 @@
+function showGameCenterModal() {
+    document.getElementById("gamecenter").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function hideGameCenterModal() {
+    document.getElementById("gamecenter").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+function showRFTG_Modal() {
+  document.getElementById("RFTG").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideRFTG_Modal() {
+  document.getElementById("RFTG").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function showLMG_Modal() {
+  document.getElementById("LMG").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideLMG_Modal() {
+  document.getElementById("LMG").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function showStructurAll_Modal() {
+  document.getElementById("StructurAll").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideStructurAll_Modal() {
+  document.getElementById("StructurAll").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function showPopix_Modal() {
+  document.getElementById("Popix").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hidePopixl_Modal() {
+  document.getElementById("Popix").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function showBPA_Modal() {
+  document.getElementById("BPA").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideBPA_Modal() {
+  document.getElementById("BPA").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
